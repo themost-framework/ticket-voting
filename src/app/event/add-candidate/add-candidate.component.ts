838c148ba6bcb0ea8711917dc455b853ae7478d0
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2020-10-12 18:44:14 +0000

    add live update

--- src/app/event/add-candidate/add-candidate.component.ts
+++ src/app/event/add-candidate/add-candidate.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core';
 import { ErrorService } from 'src/app/shared/error.service';
 import { AngularDataContext } from '@themost/angular';
 import { ActivatedRoute } from '@angular/router';
 import { LoadingService } from 'src/app/shared/loading.service';
+import { NgTemplateOutlet, DatePipe } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+import { ResponseError } from '@themost/client';
 
 @Component({
   selector: 'app-add-candidate',
@@ -12,11 +15,18 @@ import { LoadingService } from 'src/app/shared/loading.service';
 export class AddCandidateComponent implements OnInit {
 
   @Input('showShareEmail') showShareEmail = false;
+  @ViewChild('templateSubmitted') templateSubmitted: NgTemplateOutlet;
+  @ViewChild('templateInvalidPeriod') templateInvalidPeriod: NgTemplateOutlet;
+  @ViewChild('templateForm') templateForm: NgTemplateOutlet;
+  @ViewChild('templateError') templateError: NgTemplateOutlet;
 
-  public showSubmitted = false;
+  public state: NgTemplateOutlet;
+  public lastError: any;
+
 
   @Input() model = {
     description: null,
+    shortDescription: null,
     agreeTerms: false,
     agreeData: false,
     shareEmail: false,
@@ -35,18 +45,41 @@ export class AddCandidateComponent implements OnInit {
   constructor(private _errorService: ErrorService,
     private _context: AngularDataContext,
     private _activatedRoute: ActivatedRoute,
-    private _loadingService: LoadingService) { }
+    private _loadingService: LoadingService,
+    private _translateService: TranslateService) { }
 
   ngOnInit(): void {
+    this._loadingService.showLoading();
     this._activatedRoute.params.subscribe((params) => {
       Promise.all([
         this._context.model('ElectionEvents').where('identifier').equal(params.id).getItem(),
         this._context.model('ActionStatusTypes').where('alternateName').equal('PotentialActionStatus').getItem(),
       ]).then((results) => {
+        if (results[0] == null) {
+          throw new ResponseError('Election cannot be found', 400);
+        }
         // set election
         this.model.electionEvent = results[0];
         // set status
         this.model.actionStatus = results[1];
+        // validate candidacy period
+        const validPeriod = new Date() >= this.model.electionEvent.specification.candidacyFrom &&
+          new Date() <= this.model.electionEvent.specification.candidacyThrough;
+        const pipe = new DatePipe(this._translateService.currentLang);
+        this.model.electionEvent.specification.candidacyFromString = pipe.transform(this.model.electionEvent.specification.candidacyFrom, 'short');
+        this.model.electionEvent.specification.candidacyThroughString = pipe.transform(this.model.electionEvent.specification.candidacyThrough, 'short');
+        if (validPeriod === false) {
+          this.state = this.templateInvalidPeriod;
+          this._loadingService.hideLoading();
+          return;
+        }
+        // set state
+        this.state = this.templateForm;
+        this._loadingService.hideLoading();
+      }).catch((err) => {
+        this.lastError = err;
+        this._loadingService.hideLoading();
+        this.state = this.templateError;
       });
     });
   }
@@ -55,12 +88,16 @@ export class AddCandidateComponent implements OnInit {
     try {
       this._loadingService.showLoading();
       await this._context.model(`ElectionEvents/${this.model.electionEvent.identifier}/Candidates/Apply`).save(this.model);
-      this.showSubmitted = true;
+      this.state = this.templateSubmitted;
       this._loadingService.hideLoading();
     } catch (err) {
       this._loadingService.hideLoading();
       this._errorService.showError(err);
     }
+  }
+
+  public onShortDescriptionChange(event?: any) {
+
   }
 
 }
--- src/app/event/result-list/result-list.component.ts
+++ src/app/event/result-list/result-list.component.ts
@@ -15,6 +15,9 @@ export class ResultListComponent implements OnInit, OnDestroy {
     if (this.paramSubscription) {
       this.paramSubscription.unsubscribe();
     }
+    if (this.liveInterval) {
+      clearInterval(this.liveInterval);
+    }
   }
 
   constructor(private _errorService: ErrorService,
@@ -24,6 +27,28 @@ export class ResultListComponent implements OnInit, OnDestroy {
 
   public model: any;
   private paramSubscription: Subscription;
+  public updatedAt = new Date();
+  public liveUpdate = true;
+  public liveInterval;
+
+  public refresh() {
+    const id = this._activatedRoute.snapshot.params.id;
+    this._context.model(`ElectionEvents/${id}/results`).getItems().then((results) => {
+      results.votes = results.votes.sort((a, b) => {
+        if (a.total > b.total) {
+          return -1;
+        }
+        if (a.total < b.total) {
+          return 1;
+        }
+        return 0;
+      });
+      this.model = results;
+      this.updatedAt = new Date();
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 
   ngOnInit(): void {
     this.paramSubscription = this._activatedRoute.params.subscribe((params) => {
@@ -42,6 +67,11 @@ export class ResultListComponent implements OnInit, OnDestroy {
         this._errorService.showError(err);
       });
     });
+    if (this.liveUpdate) {
+      this.liveInterval = setInterval(() => {
+        this.refresh();
+      }, 1000);
+    }
   }
 
 }
